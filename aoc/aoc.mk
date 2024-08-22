9a84ea61d293b26b7c77ca208d28b1713316e33c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-08-22 12:34:59 +0300

    add missing FCM definitions for some proprietary HALs

--- aoc/aoc.mk
+++ aoc/aoc.mk
@@ -7,10 +7,8 @@ PRODUCT_PACKAGES += dump_aoc \
 PRODUCT_COPY_FILES += \
 	device/google/gs-common/aoc/conf/init.aoc.daemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.aoc.rc
 
-ifneq ($(wildcard vendor/google/whitechapel/aoc/aocx/aidl/aocx/framework_compatibility_matrix.xml),)
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    vendor/google/whitechapel/aoc/aocx/aidl/aocx/framework_compatibility_matrix.xml
-endif
+	device/google/gs-common/proprietary/aocx.IAocx.xml
 
 # AoC debug support
 PRODUCT_PACKAGES_DEBUG += \
--- device.mk
+++ device.mk
@@ -30,3 +30,5 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.software.ipsec_tunnel_migration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnel_migration.xml
+
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/proprietary/com.google.flood.xml
